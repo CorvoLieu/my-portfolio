ec5f519b0f381b8ada153c9ddac976678ca4b9ec
CorvoLieu <hao.lieu.02@gmail.com>
2026-03-13 01:29:06 +0700

    chores: remove pretier configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,46 +1,25 @@
-// eslint.config.mjs
-import eslint from "@eslint/js";
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import tsParser from "@typescript-eslint/parser";
+import astroParser from "astro-eslint-parser";
 
 export default tseslint.config(
-  // 1. Base JS recommended rules
-  eslint.configs.recommended,
-
-  // 2. TypeScript recommended rules
+  js.configs.recommended,
   ...tseslint.configs.recommended,
-
-  // 3. Astro recommended rules
   ...eslintPluginAstro.configs.recommended,
-
   {
-    // 4. Custom rules/ignores
     ignores: ["dist/", ".astro/", "node_modules/"],
   },
-
   {
     files: ["**/*.astro"],
     languageOptions: {
-      parser: eslintPluginAstro.parser,
+      parser: astroParser,
       parserOptions: {
-        parser: tseslint.parser,
+        parser: tsParser,
         extraFileExtensions: [".astro"],
-      },
-      globals: {
-        ...globals.browser, // This tells ESLint that "document" is okay to use
+        sourceType: "module",
       },
     },
-    rules: {
-      // You can add specific Astro rules here
-    },
-  },
-
-  // 5. Prettier integration
-  eslintConfigPrettier,
-  eslintPluginPrettier,
-  {
-    ignores: ["dist/", ".astro/", "node_modules/"],
   },
 );
